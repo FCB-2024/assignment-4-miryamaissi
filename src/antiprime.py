c4fba64cb612d77e8b8a32082497571498f4106c
Miryam Aissi <miryam.aissi01@estudiant.upf.edu>
2024-10-20 13:00:37 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,21 +1,44 @@
-## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
-## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
-## COMMAND LINE SPECIFIED BELOW
 def main() :
+	import sys
+
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
+	def count_divisors(n):
+		count = 0
+		for i in range(1, n + 1):
+			if n % i == 0:
+				count += 1
+		return count
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
+	def is_anti_prime(n):
+		num_divisors = count_divisors(n)
+		for i in range(1, n):
+			if count_divisors(i) >= num_divisors:
+				return False
+		return True
+
+	if len(sys.argv) != 2:
+		print("Usage: python script.py <positive_integer>")
+		return "not anti-prime"
+
+	try:
+		num = int(sys.argv[1])
+		if num <= 0:
+			raise ValueError
+	except ValueError:
+		print("Please enter a valid positive integer.")
+		return "not anti-prime"
+
+	if is_anti_prime(num):
+		return("anti-prime")
+	else:
+		return "not anti-prime"
+
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
-
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
